6a457853b066912ca176ce67e96879266c70dad1
Adnan Fiaz <adnan.fiaz@connectnationalgrid.onmicrosoft.com>
2023-01-24 22:10:35 +0000

    FEAT: added training of ldz diff model

--- src/train.py
+++ src/train.py
@@ -1,7 +1,10 @@
+import datetime as dt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import TimeSeriesSplit
+
+from prophet import Prophet
 
 
 def train(target, features):
@@ -49,3 +52,66 @@ def train_full_model(X, y):
     model.fit(X, y)
 
     return model
+
+
+def train_ldz_diff(target, features):
+    """Train a model where the target is the difference in demand and the features consist of the difference in CWV
+
+    Args:
+        target (pandas DataFrame): A dataframe with a column for the actual LDZ demand
+        features (pandas DataFrame): A dataframe with a column LDZ_DEMAND_DIFF and a column CWV_DIFF
+
+    Returns:
+        pandas Series: A series with predictions for the LDZ demand diff model
+    """
+
+    overlapping_dates = target.index.intersection(features.index)
+    y = target.loc[overlapping_dates].copy()
+    X = features.loc[overlapping_dates].copy()
+
+    data_input = X[["LDZ_DEMAND_DIFF", "CWV_DIFF"]].reset_index().dropna()
+    data_input.columns = ["ds", "y", "CWV_DIFF"]
+
+    min_date = data_input["ds"].min()
+    max_date = data_input["ds"].max()
+
+    result = []
+    # We've tried to align the method of model fitting to the method used for the Linear Regression
+    # So this is meant to mimic a weekly retraining cycle
+    # Each time the model is refitted on all historical data up to that point
+    for cutoff_date in pd.date_range(min_date, max_date, freq="7D", inclusive="neither"):
+        training_data = data_input[data_input['ds'] < cutoff_date]
+        
+        model = Prophet(
+            changepoint_prior_scale=0.001,
+            seasonality_prior_scale=0.01,
+            yearly_seasonality=False,
+            daily_seasonality=False,
+        )
+        model.add_country_holidays(country_name="UK")
+        model.add_regressor("CWV_DIFF", mode="additive", prior_scale=0.01, standardize=False)
+        
+        model = model.fit(training_data)
+
+        # this is where it gets tricky
+        # we're sort of pretending these are day ahead forecasts
+        # previous exploration into this has shown that whether it's a daily or weekly horizon makes little difference 
+        horizon = 7
+        if (max_date - cutoff_date).days + 1 < horizon:
+            horizon = (max_date - cutoff_date).days + 1
+
+        future_date = model.make_future_dataframe(periods=horizon, include_history=False)
+        mask = data_input['ds'].between(cutoff_date, cutoff_date + dt.timedelta(days=horizon-1))
+        future_date['CWV_DIFF'] = data_input.loc[mask, 'CWV_DIFF'].values
+        preds = model.predict(future_date)[['ds', 'yhat']]
+        result.append(preds)
+    
+    predictions = (pd.concat(result).
+                    rename(columns={'yhat':'PREDICTION', 'ds':'GAS_DAY'}).
+                    set_index('GAS_DAY'))
+
+    # putting the demand diff predictions back onto the same scale as the actual demand
+    predictions['PREDICTION'] += y["LDZ"].shift(2)
+
+    return predictions['PREDICTION']
+
