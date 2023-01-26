51005a385435f865cd076c75c8afec17ce787f31
Adnan Fiaz <adnan.fiaz@connectnationalgrid.onmicrosoft.com>
2023-01-26 13:48:03 +0000

    FEAT: changed method of evaluation to accomodate multiple models

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,24 +2,26 @@ import pandas as pd
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
 
-def evaluate_models(ldz_predictions, actuals):
-    ldz_predictions.name = "LDZ_PREDICTIONS"
-    combined = pd.concat([ldz_predictions, actuals], axis=1)
-
-    combined = combined.dropna()
-
-    result = pd.DataFrame(
-        {
-            "MODEL": ["LDZ"],
-            "MAE": [
-                mean_absolute_error(combined["LDZ"], combined["LDZ_PREDICTIONS"]),
-            ],
-            "MAPE": [
-                mean_absolute_percentage_error(
-                    combined["LDZ"], combined["LDZ_PREDICTIONS"]
-                ),
-            ],
-        }
+def evaluate_models(predictions, actuals):
+    combined = pd.concat([predictions, actuals["LDZ"]], axis=1)
+    result = (
+        combined.dropna()
+        .melt(
+            id_vars=["LDZ"],
+            value_vars=predictions.columns,
+            value_name="PREDICTIONS",
+            var_name="MODEL",
+        )
+        .groupby("MODEL")
+        .apply(lambda df: calculate_metrics(df))
+        .reset_index()        
     )
 
     return result
+
+
+def calculate_metrics(input_data):
+    mae = mean_absolute_error(input_data["LDZ"], input_data["PREDICTIONS"])
+    mape = mean_absolute_percentage_error(input_data["LDZ"], input_data["PREDICTIONS"])
+
+    return pd.Series({"MAE":mae, "MAPE":mape})
